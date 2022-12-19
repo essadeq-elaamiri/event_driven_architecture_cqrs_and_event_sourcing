b29d9c2dbda034373c970a40ab5a99b8502f3fc1
essadeq-elaamiri <essadeq0701@gmail.com>
2022-12-19 22:51:51 +0100

    DB

--- account-cqrs-event-sourcing/src/main/java/me/elaamiri/accountcqrseventsourcing/common_api/controllers/AccountCommandController.java
+++ account-cqrs-event-sourcing/src/main/java/me/elaamiri/accountcqrseventsourcing/common_api/controllers/AccountCommandController.java
@@ -1,10 +1,12 @@
 package me.elaamiri.accountcqrseventsourcing.common_api.controllers;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import me.elaamiri.accountcqrseventsourcing.common_api.commands.CreateAccountCommand;
 import me.elaamiri.accountcqrseventsourcing.common_api.dtos.CreatAccountRequestDTO;
 import org.axonframework.commandhandling.CommandBus;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,14 +16,15 @@ import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping(path = "/commands/account")
-@AllArgsConstructor
+@AllArgsConstructor // for injection
+@NoArgsConstructor
 public class AccountCommandController {
 
     private CommandGateway commandGateway;
 
     @RequestMapping("/create")
     public CompletableFuture<String> createAccount(@RequestBody CreatAccountRequestDTO request){
-        //
+        //asynchronous
         CompletableFuture<String> createAccountCommandResponse = commandGateway.send(new CreateAccountCommand(
                 UUID.randomUUID().toString(),
                 request.getInitialBalance(),
