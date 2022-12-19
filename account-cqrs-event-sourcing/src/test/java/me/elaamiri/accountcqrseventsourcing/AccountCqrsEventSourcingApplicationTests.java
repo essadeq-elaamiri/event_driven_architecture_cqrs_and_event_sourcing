c9a966298d07bc5530ebccbcd9b618bf28988e12
essadeq-elaamiri <essadeq0701@gmail.com>
2022-12-19 10:11:24 +0100

    init project

---
+++ account-cqrs-event-sourcing/src/test/java/me/elaamiri/accountcqrseventsourcing/AccountCqrsEventSourcingApplicationTests.java
@@ -0,0 +1,13 @@
+package me.elaamiri.accountcqrseventsourcing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountCqrsEventSourcingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
