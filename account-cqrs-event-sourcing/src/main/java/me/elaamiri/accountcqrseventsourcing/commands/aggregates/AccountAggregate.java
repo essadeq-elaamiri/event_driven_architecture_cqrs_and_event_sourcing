b079563d80bc6ef41e92f38032b33f8cdf8ee199
essadeq-elaamiri <essadeq0701@gmail.com>
2022-12-20 01:00:51 +0100

    test

--- account-cqrs-event-sourcing/src/main/java/me/elaamiri/accountcqrseventsourcing/commands/aggregates/AccountAggregate.java
+++ account-cqrs-event-sourcing/src/main/java/me/elaamiri/accountcqrseventsourcing/commands/aggregates/AccountAggregate.java
@@ -3,7 +3,9 @@ package me.elaamiri.accountcqrseventsourcing.commands.aggregates;
 import lombok.NoArgsConstructor;
 import me.elaamiri.accountcqrseventsourcing.common_api.commands.CreateAccountCommand;
 import me.elaamiri.accountcqrseventsourcing.common_api.enumerations.AccountStatus;
+import me.elaamiri.accountcqrseventsourcing.common_api.events.AccountCreatedEvent;
 import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
@@ -27,7 +29,23 @@ public class AccountAggregate {
         // Business logic
 
         if(createAccountCommand.getInitialBalance() < 0) throw new RuntimeException("Invalid Initial Balance | Negative");
-        /**AggregateLifecycle.apply( )**/
+        AggregateLifecycle.apply(new AccountCreatedEvent(
+                // Command to event
+                createAccountCommand.getId(),
+                createAccountCommand.getInitialBalance(),
+                createAccountCommand.getCurrency()
+        ));
+    }
+
+    @EventSourcingHandler
+    public void on(AccountCreatedEvent accountCreatedEvent){
+
+        // The Aggregate is the Object Status
+        this.accountId = accountCreatedEvent.getId();
+        this.balance = accountCreatedEvent.getInitialBalance();
+        this.currency = accountCreatedEvent.getCurrency();
+
+        this.status = AccountStatus.CREATED;
     }
 
 }
