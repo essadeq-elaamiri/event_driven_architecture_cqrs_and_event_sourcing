20ea9974cddf1ba25f92861e0a36c30377eaeb78
essadeq-elaamiri <essadeq0701@gmail.com>
2022-12-23 13:45:32 +0100

    dtos query

--- account-cqrs-event-sourcing/src/main/java/me/elaamiri/accountcqrseventsourcing/common_api/dtos/query/OperationResponseDTO.java
+++ account-cqrs-event-sourcing/src/main/java/me/elaamiri/accountcqrseventsourcing/common_api/dtos/query/OperationResponseDTO.java
@@ -1,4 +1,12 @@
 package me.elaamiri.accountcqrseventsourcing.common_api.dtos.query;
 
+import me.elaamiri.accountcqrseventsourcing.common_api.enumerations.OperationType;
+
+import java.util.Date;
+
 public class OperationResponseDTO {
+    private Long id;
+    private Date operationDate;
+    private double amount;
+    private OperationType operationType;
 }
